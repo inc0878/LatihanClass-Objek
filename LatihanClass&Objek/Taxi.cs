ab93564629212386d9f414c7373c285a01f21fd2
CORONA CHAN <CORONA CHAN@DESKTOP-RM3UONN>
2023-03-21 14:46:51 +0700

    Add project files.

---
+++ LatihanClass&Objek/Taxi.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObjek
+{
+    public class Taxi
+    {
+        // Properties
+        public string NAMA { get; set; }
+        public bool Bertugas { get; set; }
+        public int JML { get; set; }
+
+        // method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("\nNama driver: {0}", NAMA);
+
+            if (Bertugas == true)
+            {
+                Console.WriteLine("Sedang bertugas: Iya", Bertugas);
+            }
+            else if (Bertugas == false)
+            {
+                Console.WriteLine("Sedang bertugas: Tidak", Bertugas);
+            }
+
+            Console.WriteLine("Jumlah penumpang: {0}", JML);
+        }
+
+        public void Menjemput_penumpang()
+        {
+
+            if (Bertugas == true)
+            {
+                Console.WriteLine("\n{0} Sedang menjemput penumpang", NAMA);
+            }
+            else if (Bertugas == false)
+            {
+                Console.WriteLine("\n{0} Sedang tidak menjemput penumpang", NAMA);
+            }
+        }
+
+        public void Menurunkan_penumpang()
+        {
+            if (Bertugas == true)
+            {
+                Console.WriteLine("{0} belum selesai mengantar penumpang", NAMA);
+            }
+            else if (Bertugas == false)
+            {
+                Console.WriteLine("{0} Selesai mengantar penumpang", NAMA);
+            }
+        }
+
+    }
+}
---
+++ LatihanClass&Objek/program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObjek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Taxi
+            Taxi taxi = new Taxi();
+            Taxi taksi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.NAMA = "Hassan";
+            taxi.Bertugas = true;
+            taxi.JML = 5;
+
+            taksi.NAMA = "dono";
+            taksi.Bertugas = false;
+            taksi.JML = 0;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.Menjemput_penumpang();
+            taxi.Menurunkan_penumpang();
+
+            taksi.TaxiInfo();
+            taksi.Menjemput_penumpang();
+            taksi.Menurunkan_penumpang();
+
+            Console.ReadKey();
+        }
+    }
+}
